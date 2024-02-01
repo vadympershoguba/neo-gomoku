a6fd87bae124e4d8676eb287011c3ae019b606cc
Vadym Pershoguba <tanakenrip@gmail.com>
2024-02-01 15:23:41 +0100

    01.02 15:23

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,59 @@ window.onload = () => {
             td.onclick = ()=> {
                 td.style.backgroundColor = 'red'
                 gameRecord[td.id] = 1;
+                checkWin()
             };
             tr.appendChild(td)
             counter++;
         }
         document.getElementById('board-table').appendChild(tr)
+    }
+}
+
+function showClose(id) {
+    const coordinates = [+id-1, +id+1, +id-14, +id-15, +id-16, +id+14, +id+15, +id+16]
+    for (let i = 0; i < coordinates.length; i++) {
+        colorCell(coordinates[i])
+    }
+}
+
+function colorCell(id) {
+    const cell = document.getElementById(id)
+    if (cell.style.backgroundColor != 'red'){
+        cell.style.backgroundColor = 'yellow'
+    }
+}
+
+class Game {
+
+    constructor () {
+        this.board = gameRecord;
+        this.activeTurn = 'user';
+    }
+
+    over(){
+
+    }
+}
+
+function checkWin () {
+    for (let i = 0; i < gameRecord.length; i++) {
+        if (checkHorizontalLeft(gameRecord[i]) == 1){
+            alert(123)
+            break
+        }
+    }
+    function checkHorizontalLeft(id) {
+        console.log(id)
+        if (id - 3 > 0) {
+            for (let j = 1; j < 5; j++){
+                if (gameRecord[id-j] != 1){
+                    return 0;
+                }
+                else {
+                    return 1;
+                }
+            }
+        }
     }
 }
