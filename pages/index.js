45b40b16f2948d8d00a1bed3dbf92f22db2671dd
Vadym Pershoguba <tanakenrip@gmail.com>
2024-02-01 22:46:06 +0200

    01.02 21:46

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ window.onload = () => {
             td.onclick = ()=> {
                 td.style.backgroundColor = 'red'
                 gameRecord[td.id] = 1;
-                checkWin()
+                checkWin(td.id, 1)
             };
             tr.appendChild(td)
             counter++;
@@ -59,24 +59,57 @@ class Game {
     }
 }
 
-function checkWin () {
+function checkWin (id, value) {
+    id = +id;
     for (let i = 0; i < gameRecord.length; i++) {
-        if (checkHorizontalLeft(gameRecord[i]) == 1){
-            alert(123)
-            break
+        if (checkHorizontalLeft(id) == value && id % 15 > 3){
+            return 1
+        }
+        if (checkHorizontalRight(id) == value){
+            return 1
+        } 
+        if (checkVerticalUp(id) == value){
+            return 1
+        } 
+        if (checkVerticalDown(id) == value) {
+            return 1
         }
     }
-    function checkHorizontalLeft(id) {
-        console.log(id)
+    function checkHorizontalLeft() {
         if (id - 3 > 0) {
             for (let j = 1; j < 5; j++){
-                if (gameRecord[id-j] != 1){
+                if (gameRecord[id-j] != value){
                     return 0;
                 }
-                else {
-                    return 1;
-                }
             }
+            return 1
         }
     }
+    function checkHorizontalRight() {
+        if (id - 3 > 0) {
+            for (let j = 1; j < 5; j++){
+                if (gameRecord[id+j] != value){
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+    function checkVerticalUp() {
+        for (let j = 0; j <=60; j+=15) {
+            if (gameRecord[id-j] != value){
+                return 0
+            }
+        }
+        return 1
+    }
+    function checkVerticalDown() {
+        for (let j = 0; j <=60; j+=15) {
+            if (gameRecord[id+j] != value){
+                return 0
+            }
+        }
+        return 1
+    }
+    return 0
 }
